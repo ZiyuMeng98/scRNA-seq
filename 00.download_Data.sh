b80846a51a97762f34f65c4b6320e9249466c77c
ZiyuMeng98 <154886834+ZiyuMeng98@users.noreply.github.com>
2026-02-27 14:01:33 +0800

    Enhance download_Data.sh for GSE26887 dataset
    Added commands to download and extract GSE26887 dataset.

--- 00.download_Data.sh
+++ 00.download_Data.sh
@@ -11,3 +11,9 @@ mkdir conversion_verification
 cd conversion_verification
 echo -e "SRR28574088\nSRR28574089\nSRR28574090\nSRR28574091\nSRR28574092\nSRR28574093\nSRR28574094\nSRR28574095\nSRR28574096\nSRR28574097\nSRR28574098\nSRR28574099\nSRR28574100\nSRR28574101\nSRR28574102\nSRR28574103\nSRR28574104\nSRR28574105\nSRR28574106\nSRR28574107\nSRR28574108" > list.txt
 nohup bash -c 'cat list.txt | parallel -j 21 "prefetch {}"' > download.log 2>&1 &
+wget -c -O GSE26887_RAW.tar 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE26887&format=file'
+tar -xvf GSE26887_RAW.tar
+mv -r GSM* raw_GSM/
+mkdir raw_GSM
+cd raw_GSM
+gunzip *
